76213f86e4d89de7d7945b7cf8d64b6bb90f6b7f
harishgwalani <harish.gwalani@gmail.com>
2022-12-02 12:23:38 +0530

    second commit

--- Even_Odd.java
+++ Even_Odd.java
@@ -8,16 +8,20 @@ public class Even_Odd {
 		// TODO Auto-generated method stub
 		int number;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number: ");
-		number = sc.nextInt();
-		if(number%2==0)
-		{
-			System.out.println("Number is Even");
-		}
-		else
-		{
-			System.out.println("Number is not even");
-		}
-	}
+	
+		String c = "";
+		do {
+			System.out.println("Enter number: ");
+			number = sc.nextInt();
+			if (number % 2 == 0) {
+				System.out.println("Number is Even");
+			} else {
+				System.out.println("Number is not even");
+			}
 
+			System.out.println("Do you want to continue y or n");
+			c = sc.nextLine();
+			System.out.println(c);
+		} while(c.equalsIgnoreCase("Y"));
+	}
 }
